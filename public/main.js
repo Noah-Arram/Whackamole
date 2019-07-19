a64e3d7f6197d18452bcba9cb8cba8c15bc5f65d
noah <noah@2simple.com>
2019-07-19 13:06:48 +0100

    spawner and utilities js pages done

--- public/main.js
+++ public/main.js
@@ -16,6 +16,9 @@ var basicnumbers = [];
 var titles = [];
 var backtms = [];
 var backs = [];
+var offsetx  = -7.5
+var offsety = -80
+var helpcontent = ["The odd numbers are all the numbers that exist \n that cannot be divided by 2. \n EG 3 is an odd number as if you have 3 blocks \n you can’t split them into 2 piles of the same amount \n each. 4 is not an odd number as if you have 4 blocks \n you could split them into 2 piles of the same amount \n each.","The even numbers are all the numbers that exist \n that can be divided by 2. \n EG 4 is an even number as if you have 4 blocks \n you can split them into 2 piles of the same amount \n each. 3 is not an even number as if you have 3 blocks \n you can’t split them into 2 piles of the same amount \n each.","Square numbers are numbers that have been \n made by multiplying a whole number by itself. \n 2 EG 4 is a square number as it can be made \n by multiplying a whole number by itself, in this \n case that number is 2 (2x2=4). 5 is not a square \n number as it cannot be made by multiplying a whole \n number by itself.","Prime numbers are numbers can only be divided \n by themselves and by 1 to get an answer that is \n a whole number.  EG 7 is a prime number as the \n only numbers that it can be divided by to make \n a whole number is 7 (itself) and by 1. 8 is not a \n prime number as it can be divided by 1, 2 4 and \n eight to make a whole number. NOTE- 1 is not \n a prime number even though it seems to meet \n the requirements for a number to be a prime \n number.  This is because a prime number must be \n able to divide by 2 numbers, itself and 1. The \n number 1 can only divide by 1 number as itself and \n 1 are the same thing."];
 var scale;
 var scoretext = [];
 var positions= [[[100,100], [450,100], [800, 100]],[[100,300], [450,300], [800, 300]],[[100,500], [450,500], [800, 500]]]
@@ -100,17 +103,17 @@ if (gametype [0] === true)
 		{
 		backs[0].visible = true;
 	 labels[l].visible = true;
-	 helptexts[l].visible = false;
+	 helptexts[0].visible = false;
  }
 	}
 	for (let i = 0; i < 4; i++)
 	{
 	if (helptype[i] === true)
 	{
-		helptexts[i].visible = (helptype[i] === true);
-		for (let l = 0; l < 4; l++)
+		helptexts[0].text = "" + helpcontent[i];
+		helptexts[0].visible = true;
 		{
-		labels[l].visible = false;
+		labels[i].visible = false;
 	}
 	}
 }
@@ -136,199 +139,19 @@ spawn.createTitle()
 
 spawn.createhelpbutton()
 
-var back = new createjs.Bitmap("back.png");
-stage.addChild(back);
-back.x = 465;
-back.y = 650;
-backs.push(back);
-back.addEventListener("click", function(event)
-{
-	if (gametype [2] === true)
-	{
-	gametype [2] = false;
-	gametype [1] = true;
-  }
-else
-{
-	gametype [2] = true;
-	for (let l = 0; l < 4; l++)
-	{
-		labels[l].visible = true;
-	helptexts[l].visible = false;
-	helptype[l] = false;
-  }
-}
+spawn.createbackbutton()
 
-})
+spawn.createhelptext()
 
-var hodd = new createjs.Text("The odd numbers are all the numbers that exist \n that cannot be divided by 2. \n EG 3 is an odd number as if you have 3 blocks \n you can’t split them into 2 piles of the same amount \n each. 4 is not an odd number as if you have 4 blocks \n you could split them into 2 piles of the same amount \n each.", "40px comic sans MS", "#00000");
-hodd.x = 100;
-hodd.y = 100;
-hodd.visible = false;
-stage.addChild(hodd);
-helptexts.push(hodd);
+spawn.createscore()
 
-var heven = new createjs.Text("The even numbers are all the numbers that exist \n that can be divided by 2. \n EG 4 is an even number as if you have 4 blocks \n you can split them into 2 piles of the same amount \n each. 3 is not an even number as if you have 3 blocks \n you can’t split them into 2 piles of the same amount \n each.", "40px comic sans MS", "#00000");
-heven.x = 100;
-heven.y = 75;
-heven.visible = false;
-stage.addChild(heven);
-helptexts.push(heven);
+spawn.createholes()
 
-var hsquare = new createjs.Text("Square numbers are numbers that have been \n made by multiplying a whole number by itself. \n 2 EG 4 is a square number as it can be made \n by multiplying a whole number by itself, in this \n case that number is 2 (2x2=4). 5 is not a square \n number as it cannot be made by multiplying a whole \n number by itself.", "40px comic sans MS", "#00000");
-hsquare.x = 100;
-hsquare.y = 75;
-hsquare.visible = false;
-stage.addChild(hsquare);
-helptexts.push(hsquare);
+spawn.createmoles()
 
-var hprime = new createjs.Text("Prime numbers are numbers can only be divided \n by themselves and by 1 to get an answer that is \n a whole number.  EG 7 is a prime number as the \n only numbers that it can be divided by to make \n a whole number is 7 (itself) and by 1. 8 is not a \n prime number as it can be divided by 1, 2 4 and \n eight to make a whole number. NOTE- 1 is not \n a prime number even though it seems to meet \n the requirements for a number to be a prime \n number.  This is because a prime number must be \n able to divide by 2 numbers, itself and 1. The \n number 1 can only divide by 1 number as itself and \n 1 are the same thing.", "40px comic sans MS", "#00000");
-hprime.x = 100;
-hprime.y = 75;
-hprime.visible = false;
-stage.addChild(hprime);
-helptexts.push(hprime);
+spawn.createresetbutton()
 
-	var text = new createjs.Text("Score: 0", "40px comic sans MS", "#00000");
-	 text.x = 50;
-	 text.y = 700;
-	 text.textBaseline = "alphabetic";
-	 stage.addChild(text);
-scoretext.push(text);
-
-
-var offsetx  = -7.5
-var offsety = -80
-
-for (let i = 0 ;i < 3 ;i++)
-{
-for (let j=0 ;j < 3; j++)
-{
-	var hole = new createjs.Shape();
-	hole.graphics.beginFill("black").drawEllipse(0, 0, 125, 75);
-	hole.x = positions[i][j][0];
-	hole.y = positions[i][j][1];
-	stage.addChild(hole);
-	holes.push(hole);
-}
-}
-
-
-
-for (let i = 0 ;i < 3 ;i++)
-{
-for (let j=0 ;j < 3; j++)
-{
-	var mole = new createjs.Bitmap("mole.png");
-	stage.addChild(mole);
-	mole.x = positions[i][j][0] + offsetx;
-	mole.y = positions[i][j][1] + offsety;
-	mole._index = moles.length;
-	moles.push(mole);
-	mole.visible = false;
-	mole.addEventListener("click", function(event) {
- if (gamemode[0] === true)
-	{
-  if ((randomlist[event.target._index] + 1) % 2 === 1)
-	    {
-		    score = score + 1;
-		    helpers.updateScore();
-		  event.target.visible = false;
-      }
-			else if ((randomlist[event.target._index] + 1) % 2 === 0)
-			{
-				if (score !== 0)
-				{
-				score = score - 1;
-				helpers.updateScore();
-			  }
-				event.target.visible = false;
-			}
-	  	}
-	  if (gamemode[1] === true)
-		{
-			if ((randomlist[event.target._index] + 1) % 2 === 0)
-			    {
-				    score = score + 1;
-				    helpers.updateScore();
-				  event.target.visible = false;
-		      }
-					else if ((randomlist[event.target._index] + 1) % 2 === 1)
-					{
-						if (score !== 0)
-						{
-						score = score - 1;
-						helpers.updateScore();
-					 }
-						event.target.visible = false;
-					}
-			  	}
-					else if (gamemode[2] === true)
-						{
-						if(randomlist[event.target._index] + 1 === 1||randomlist[event.target._index] + 1 === 4||randomlist[event.target._index] + 1 === 9)
-									{
-										score = score + 1;
-										helpers.updateScore();
-									event.target.visible = false;
-									}
-									else
-									{
-										if (score !== 0)
-										{
-										score = score - 1;
-										helpers.updateScore();
-									 }
-										event.target.visible = false;
-									}
-						}
-						else if (gamemode[3] === true)
-							{
-							if(randomlist[event.target._index] + 1 === 2||randomlist[event.target._index] + 1 === 3||randomlist[event.target._index] + 1 === 5||randomlist[event.target._index] + 1 === 7)
-										{
-											score = score + 1;
-											helpers.updateScore();
-										event.target.visible = false;
-										}
-										else
-										{
-											if (score !== 0)
-											{
-											score = score - 1;
-											helpers.updateScore();
-										 }
-											event.target.visible = false;
-										}
-							}
-		})
-	}
-
-
-  }
-
-var reset = new createjs.Bitmap("reset.png");
-reset.x = 850;
-reset.y = 680;
-resets.push(reset);
-stage.addChild(reset);
-reset.addEventListener("click", function(event) {
-	score = 0;
-	helpers.updateScore()
-
-})
-
-var backtm = new createjs.Bitmap("backtm.png");
-backtm.x = 450;
-backtm.y = 680;
-backtms.push(backtm);
-stage.addChild(backtm);
-backtm.addEventListener("click", function(event) {
-	gametype [1] = true;
-	gametype [0] = false;
-  for (let i = 0; i < 4; i++)
-	{
-		gamemode[i] = false;
-	}
-})
+spawn.createbtgbutton()
 }
 
 
--- public/spawner.js
+++ public/spawner.js
@@ -60,5 +60,188 @@ createhelpbutton:function()
   	gametype [1] = false;
   })
 },
+createbackbutton:function()
+{
+  var back = new createjs.Bitmap("back.png");
+  stage.addChild(back);
+  back.x = 465;
+  back.y = 650;
+  backs.push(back);
+  back.addEventListener("click", function(event)
+  {
+  	if (gametype [2] === true)
+  	{
+  	gametype [2] = false;
+  	gametype [1] = true;
+    }
+  else
+  {
+  	gametype [2] = true;
+  	for (let l = 0; l < 4; l++)
+  	{
+  		labels[l].visible = true;
+  	helptexts[0].visible = false;
+  	helptype[l] = false;
+    }
+  }
 
+  })
+
+},
+createscore:function()
+{
+  var text = new createjs.Text("Score: 0", "40px comic sans MS", "#00000");
+   text.x = 50;
+   text.y = 700;
+   text.textBaseline = "alphabetic";
+   stage.addChild(text);
+scoretext.push(text);
+
+},
+createhelptext:function()
+{
+  var helpname = new createjs.Text("", "40px comic sans MS", "#00000");
+  helpname.x = 75;
+  helpname.y = 100;
+  helpname.visible = false;
+  stage.addChild(helpname);
+  helptexts.push(helpname);
+},
+createholes:function()
+{
+  for (let i = 0 ;i < 3 ;i++)
+  {
+  for (let j=0 ;j < 3; j++)
+  {
+  	var hole = new createjs.Shape();
+  	hole.graphics.beginFill("black").drawEllipse(0, 0, 125, 75);
+  	hole.x = positions[i][j][0];
+  	hole.y = positions[i][j][1];
+  	stage.addChild(hole);
+  	holes.push(hole);
+  }
+  }
+},
+createmoles:function()
+{
+  for (let i = 0 ;i < 3 ;i++)
+  {
+  for (let j=0 ;j < 3; j++)
+  {
+  	var mole = new createjs.Bitmap("mole.png");
+  	stage.addChild(mole);
+  	mole.x = positions[i][j][0] + offsetx;
+  	mole.y = positions[i][j][1] + offsety;
+  	mole._index = moles.length;
+  	moles.push(mole);
+  	mole.visible = false;
+  	mole.addEventListener("click", function(event) {
+   if (gamemode[0] === true)
+  	{
+    if ((randomlist[event.target._index] + 1) % 2 === 1)
+  	    {
+  		    score = score + 1;
+  		    helpers.updateScore();
+  		  event.target.visible = false;
+        }
+  			else if ((randomlist[event.target._index] + 1) % 2 === 0)
+  			{
+  				if (score !== 0)
+  				{
+  				score = score - 1;
+  				helpers.updateScore();
+  			  }
+  				event.target.visible = false;
+  			}
+  	  	}
+  	  if (gamemode[1] === true)
+  		{
+  			if ((randomlist[event.target._index] + 1) % 2 === 0)
+  			    {
+  				    score = score + 1;
+  				    helpers.updateScore();
+  				  event.target.visible = false;
+  		      }
+  					else if ((randomlist[event.target._index] + 1) % 2 === 1)
+  					{
+  						if (score !== 0)
+  						{
+  						score = score - 1;
+  						helpers.updateScore();
+  					 }
+  						event.target.visible = false;
+  					}
+  			  	}
+  					else if (gamemode[2] === true)
+  						{
+  						if(randomlist[event.target._index] + 1 === 1||randomlist[event.target._index] + 1 === 4||randomlist[event.target._index] + 1 === 9)
+  									{
+  										score = score + 1;
+  										helpers.updateScore();
+  									event.target.visible = false;
+  									}
+  									else
+  									{
+  										if (score !== 0)
+  										{
+  										score = score - 1;
+  										helpers.updateScore();
+  									 }
+  										event.target.visible = false;
+  									}
+  						}
+  						else if (gamemode[3] === true)
+  							{
+  							if(randomlist[event.target._index] + 1 === 2||randomlist[event.target._index] + 1 === 3||randomlist[event.target._index] + 1 === 5||randomlist[event.target._index] + 1 === 7)
+  										{
+  											score = score + 1;
+  											helpers.updateScore();
+  										event.target.visible = false;
+  										}
+  										else
+  										{
+  											if (score !== 0)
+  											{
+  											score = score - 1;
+  											helpers.updateScore();
+  										 }
+  											event.target.visible = false;
+  										}
+  							}
+  		})
+  	}
+
+
+    }
+},
+createresetbutton:function()
+{
+  var reset = new createjs.Bitmap("reset.png");
+  reset.x = 850;
+  reset.y = 680;
+  resets.push(reset);
+  stage.addChild(reset);
+  reset.addEventListener("click", function(event) {
+  	score = 0;
+  	helpers.updateScore()
+
+  })
+
+},
+createbtgbutton:function()
+{
+  var backtm = new createjs.Bitmap("backtm.png");
+  backtm.x = 450;
+  backtm.y = 680;
+  backtms.push(backtm);
+  stage.addChild(backtm);
+  backtm.addEventListener("click", function(event) {
+  	gametype [1] = true;
+  	gametype [0] = false;
+    for (let i = 0; i < 4; i++)
+  	{
+  		gamemode[i] = false;
+  	}
+  })
+  }
 }
